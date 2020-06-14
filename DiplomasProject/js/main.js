535ab858a0e39842c6fe05ad55247ee06b47c99e
vladyslavkozlov2101git <vladyslavkozlov2101git@gmail.com>
2020-06-14 18:32:09 +0300

    added voyage

---
+++ DiplomasProject/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+    
+    // var mixer = mixitup('.works__items');
+
+    var mixer = mixitup('.works__items', {
+        animation: {
+            duration: 500,
+            effects: 'fade scale(0.5)'
+        }
+    });
+    
+
+});
+
