db42a4d519cbcb168631c6b144ce0063d71c2952
vladyslavkozlov2101git <vladyslavkozlov2101git@gmail.com>
2020-06-17 20:22:54 +0300

    added video

--- DiplomasProject/js/main.js
+++ DiplomasProject/js/main.js
@@ -12,3 +12,31 @@ $(function(){
 
 });
 
+var video = document.querySelector('.video');
+var circle = document.querySelector('.circle');
+var juice = document.querySelector('.oranje__juice');
+var btn = document.getElementById('play-pause');
+
+
+function togglePlayPause(){
+    if (video.paused){
+        btn.className = 'pause';
+        video.play ();
+    }
+    else{
+        btn.className = "play";
+        video.pause ();
+    }
+}
+
+btn.onclick = function () {
+    togglePlayPause();
+};
+
+video.addEventListener('timeupdate', function(){
+    circlePos = video.currentTime / video.duration;
+    circle.style.left = circlePos * 100 +'%';
+    if (video.ended){
+        btn.className = 'play';
+    }
+})
