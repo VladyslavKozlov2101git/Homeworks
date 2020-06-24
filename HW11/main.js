3341ecb37d333ab9ffd815dc8cae86b1024f7510
VladyslavKozlov2101git <vladislavkozlov2101git@gmail.com>
2020-06-24 18:39:33 +0300

    finished 3rd task

--- HW11/main.js
+++ HW11/main.js
@@ -21,16 +21,49 @@ function fib(n){
 
 //TASK 2
 let n1 = 2000,
-    i = 0;
+    num = 0;
 
 function division (n1){
     while (n1 >= 50){
         n1 /= 2;
-        i++;      
+        num++;      
         
     }
-    console.log(i - 1);
+    console.log(num - 1);
 }
 
 division (n1)
 
+//TASK 3
+
+
+let cabinet = {
+    name : 'Владислав',
+    tel : '0994007259',
+    address : 'вул. Пушкіна, буд. 2',
+    totalWeight : 2700,
+    totalCosts : 830,
+    items:{
+        item1:{
+            name: 'Чашка',
+            price: 100,
+            weight: 200
+        },
+        item2:{
+            name: 'ложка',
+            price: 30,
+            weight: 100
+        },
+        item3:{
+            name: 'Чайник',
+            price: 550,
+            weight: 2000
+        },
+        item4:{
+            name: 'Кружка',
+            price: 150,
+            weight: 400
+        }
+    }
+}
+console.log(cabinet.items.item2.weight)
