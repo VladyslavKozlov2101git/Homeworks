98e98b262438f593245ebe12080cc6105ed796ef
VladyslavKozlov2101git <vladislavkozlov2101git@gmail.com>
2020-06-25 19:16:49 +0300

    fixed

--- HW11/main.js
+++ HW11/main.js
@@ -3,18 +3,17 @@
 //TASK 1
 
 
-let n;
-
-function fib(n){
-    if (n==1||n==2)
-    return 1;
-
-    else
-    return fib(n-2)+fib(n-1);}
-
-    let number=+prompt('Введіть порядковий номер');
-    console.log(fib(number));
-
+function fib(n) {
+    let a = 1;
+    let b = 1;
+      for (let i = 3; i <= n; i++) {
+      let c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
+  }
+console.log(fib(10));
 
 
 
